500fa3620387a3afbfc03d3125d8a2410d746af4
Edward Wang <edwardwang9966@gmail.com>
2025-04-08 19:33:05 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,78 @@
+import "./style.css"
+import {getweather} from "./weather.js"
+import {ICON_MAP} from "./iconmap.js"
+
+navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
+function positionSuccess(position){
+    getweather(position.coords.latitude,position.coords.longitude,Intl.DateTimeFormat().resolvedOptions().timezone).then(renderWeather).catch(e => {
+        console.error(e);
+        alert(e);
+    })
+}
+function positionError(){
+    alert("we couldn't find you")
+}
+
+
+
+function renderWeather({current,daily,hourly}) {
+    renderCurrentWeather(current),
+    renderDailyWeather(daily),
+    renderHourlyWeather(hourly),
+    document.body.classList.remove("blurred")
+}
+
+function setValue(selector, value, {parent = document} = {}){
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
+
+function getIconUrl(iconcode){
+    return `/icons/${ICON_MAP.get(iconcode)}.svg`
+}
+
+const currentIcon = document.querySelector("[data-current-icon]")
+function renderCurrentWeather(current){
+    currentIcon.src = getIconUrl(current.iconCode)
+    setValue("current-temp", current.currentTemp)
+    setValue("current-high", current.highTemp)
+    setValue("current-low", current.lowTemp)
+    setValue("current-fl-high", current.highFeelsLike)
+    setValue("current-fl-low", current.lowFeelsLike)
+    setValue("current-wind", current.windSpeed)
+    setValue("current-precipitation", current.precip)
+
+}
+
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, {weekday: "long"})
+const dailySection = document.querySelector("[data-day-section]")
+const dayCardTemplate = document.getElementById("day-card-template")
+function renderDailyWeather(daily){
+    dailySection.innerHTML = ""
+    daily.forEach(day => {
+        const element = dayCardTemplate.content.cloneNode(true)
+        setValue("temp", day.maxTemp, {parent: element})
+        setValue("day", DAY_FORMATTER.format(day.timestamp), {parent: element})
+        setValue("precip", day.precipitation, {parent: element})
+        element.querySelector("[data-icon]").src = getIconUrl(day.iconCode)
+        dailySection.append(element)
+    })
+}
+
+const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, {hour: "numeric", hour12: true})
+const hourlySection = document.querySelector("[data-hour-section]")
+const hourRowTemplate = document.getElementById("hour-row-template")
+function renderHourlyWeather(hourly){
+    hourlySection.innerHTML = ""
+    hourly.forEach(hour => {
+        const element = hourRowTemplate.content.cloneNode(true)
+        setValue("temp", hour.temp, {parent: element})
+        setValue("fl-temp", hour.feelsLike, {parent: element})
+        setValue("wind", hour.windSpeed, {parent: element})
+        setValue("precip", hour.precip, {parent: element})
+        setValue("precip-chance", hour.precip_chance, {parent: element})
+        setValue("day", DAY_FORMATTER.format(hour.timestamp), {parent: element})
+        setValue("time", HOUR_FORMATTER.format(hour.timestamp), {parent: element})
+        element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode)
+        hourlySection.append(element)
+    })
+}
---
+++ src/weather.js
@@ -0,0 +1,78 @@
+import axios from "axios"
+
+export function getweather(lat, lon, timezone){
+    //https://api.open-meteo.com/v1/forecast?daily=weather_code,temperature_2m_max,apparent_temperature_max,precipitation_sum,temperature_2m_min,apparent_temperature_min&hourly=temperature_2m,apparent_temperature,weather_code,precipitation,wind_speed_10m&current_weather=true&timeformat=unixtime&latitude=52.52&longitude=13.41
+    console.log(lat);
+    console.log(lon);
+    return axios.get(
+        "https://api.open-meteo.com/v1/forecast?daily=weather_code,temperature_2m_max,precipitation_sum,apparent_temperature_max,precipitation_sum,temperature_2m_min,apparent_temperature_min&hourly=temperature_2m,apparent_temperature,weather_code,precipitation,wind_speed_10m,precipitation_probability&current_weather=true&timeformat=unixtime", {
+            params:{
+                latitude: lat,
+                longitude: lon,
+                timezone,
+            }
+        }
+    )
+    .then(({ data }) =>{
+        return{
+            data: data,
+            current: parseCurrentWeather(data),
+            daily: parseDailyWeather(data),
+            hourly: parseHourlyWeather(data)
+        }
+    })
+        
+}
+
+function parseCurrentWeather({current_weather, daily}){
+    const { 
+        temperature: currentTemp, 
+        windspeed: windSpeed, 
+        weathercode: iconCode
+    } = current_weather
+    const{
+        temperature_2m_max: [maxTemp],
+        temperature_2m_min: [minTemp],
+        apparent_temperature_max: [maxFeelsLike],
+        apparent_temperature_min: [minFeelsLike],
+        precipitation_sum: [precip],
+    } = daily
+    return {
+        currentTemp: currentTemp,
+        highTemp: maxTemp,
+        lowTemp: minTemp,
+        highFeelsLike: maxFeelsLike,
+        lowFeelsLike: minFeelsLike,
+        windSpeed: windSpeed,
+        precip: precip,
+        iconCode,
+    }
+}
+  
+
+function parseDailyWeather({daily}){
+    return daily.time.map((time,index) =>{
+        return{
+            timestamp: time*1000,
+            iconCode: daily.weather_code[index],
+            maxTemp: daily.temperature_2m_max[index],
+            maxTemp: daily.temperature_2m_max[index],
+            precipitation: daily.precipitation_sum[index]
+        }
+    })
+}
+
+function parseHourlyWeather({hourly, current_weather}){
+    return hourly.time.map((time,index) => {
+        return{
+            timestamp: time*1000,
+            iconCode: hourly.weather_code[index],
+            temp: hourly.temperature_2m[index],
+            feelsLike: hourly.apparent_temperature[index],
+            windSpeed: hourly.wind_speed_10m[index],
+            precip: hourly.precipitation[index],
+            precip_chance: hourly.precipitation_probability[index]
+        }
+    }).filter(({timestamp}) => timestamp >= current_weather.time * 1000)
+
+}
