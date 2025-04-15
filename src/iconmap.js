6e0c5153dec31f9127f8913e5d03fae675f2e420
Edward Wang <edwardwang9966@gmail.com>
2025-04-15 10:48:06 -0400

    fixed weather icons

--- src/iconmap.js
+++ src/iconmap.js
@@ -1,12 +1,12 @@
 export const ICON_MAP = new Map()
 
-addMapping([0,1], "sun")
-addMapping([2], "cloud-sun")
-addMapping([3], "cloud")
-addMapping([45,48], "smog")
-addMapping([51, 53, 55, 56, 57, 61, 63, 65, 80, 81, 82], "cloud-showers-heavy")
-addMapping([71,73,75,77,85,86], "snowflake")
-addMapping([95,96,99], "sun")
+addMapping([0,1,2], "sun")
+addMapping([3], "cloud-sun")
+addMapping([4,5,19], "cloud")
+addMapping([40,41,42,43,44,45,46,47,48,49], "smog")
+addMapping([51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99], "cloud-showers-heavy")
+addMapping([71,72,73,74,75,76,77,78,79], "snowflake")
+addMapping([95,96,97,98,99], "cloud-bolt");
 
 function addMapping(values,icon){
     values.forEach(value =>{
